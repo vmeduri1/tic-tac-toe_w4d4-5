861961166fe45883b1d159a66a06ce9f579f85fc
Vishaal Meduri <vishaal.meduri@gmail.com>
2021-02-04 17:27:38 -0600

    Madepreliminary changes

---
+++ tic-tac-toe.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContenLoaded", () => {
+    let currentPlayerSymbol = "X";
+    let squareValues = ["","","","","","","","",""];
+    document
+        .getElementById("tic-tac-toe-board")
+        .addEventListener("click", e => {
+            console.log("A square is clicked:", e.target.id);
+        })
+});
